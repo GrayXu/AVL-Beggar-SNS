749e35098aae98c80b34bb62161bde7ce4b38837
Gray <285293728@qq.com>
2018-02-12 01:11:34 +0800

    Update AvlCal.h
    finish ver 1.0 without any test

--- AvlCal.h
+++ AvlCal.h
@@ -1,7 +1,10 @@
 #include <AvlUtils.h>
 void traInsertAVL(FNode * node);
 
-//reNode would lead a AVL-Tree of friends set. GLOBAL
+/*
+reNode would lead a AVL-Tree of friends set. GLOBAL
+MUST BE FREE after show
+*/
 FNode * reNode;
 
 /**
@@ -30,10 +33,10 @@ void getSecFriend_U2T(UNode * uNode) {
 }
 
 /**
-Set_union. 
+SET_UNION. 
 Get common friend for SNS.
 */
-FNode * getComFriend(FNode * aFNode, FNode * bFNode) {
+FNode * getSetUnion(FNode * aFNode, FNode * bFNode) {
 	reNode = NULL;
 	traInsertAVL(aFNode);
 	traInsertAVL(bFNode);
@@ -56,9 +59,9 @@ void traInsertAVL(FNode * node) {
 
 /**
 set_intersection to find common friend.
-And the return-FNode should be destroyed.
+And the return-FNode should be destroyed outside.
 */
-FNode * getIntersec(FNode * aFNode, FNode * bFNode) {
+FNode * getSetIntersec(FNode * aFNode, FNode * bFNode) {
 	reNode = NULL;
 	getIntersec_check_insert(aFNode, bFNode);
 	//get returning node
@@ -74,7 +77,7 @@ void getIntersec_check_insert(FNode * node, FNode * sampleRoot) {
 		return;
 	} else {
 		FNode * resultNode = getFNodeFromName_F(node->info->name, sampleRoot);
-		if (resultNode) {//it exists
+		if (resultNode) {//exist
 			insertAVL_F(resultNode->info, reNode, 0);
 		}
 		getIntersec_check_insert(node->left, sampleRoot);
@@ -86,49 +89,88 @@ void getIntersec_check_insert(FNode * node, FNode * sampleRoot) {
 set_equal.
 1->equal, 0->not equal
 */
-int isEqual(FNode * aFNode, FNode * bFNode) {
-	FNode * resultNode = getIntersec(aFNode, bFNode);
+int setEqual(FNode * aFNode, FNode * bFNode) {
+	FNode * resultNode = getSetIntersec(aFNode, bFNode);
+	int flag = 0;
 	if (resultNode) {
-		return 1;
-	} else {
-		return 0;
+		flag = 1;
 	}
+	destroyAVL_F(resultNode);//free this temporary tree
+	
+	return flag;
+}
+
+
+/*
+Set_diffrence -> a-b, 
+Traverse each node of a=tree, find out if it's exist in b-tree or not.
+*/
+FNode * getSetDifference(FNode * aFNode, FNode * bFNode) {
+	reNode = NULL;
+	getSetDiff_recursion(aFNode, bFNode);
+	return reNode;
+}
+/**
+Traverse a FNode-AVL-Tree, check whether it exist in another F-A-L or not. yes->insert into returning tree
+Call this func outside with a root node to be para "node"
+*/
+void getSetDiff_recursion(FNode * minusNode, FNode * sampleRoot) {
+	if (minusNode == NULL) {
+		return;
+	} else {
+		FNode * resultNode = getFNodeFromName_F(minusNode->info->name, sampleRoot);
+		if (resultNode == NULL) {//not exist
+			insertAVL_F(resultNode->info, reNode, 0);
+		}
+		getSetDiff_recursion(minusNode->left, sampleRoot);
+		getSetDiff_recursion(minusNode->right, sampleRoot);
+	}
+}
+
+/**
+set_subset
+If a is subset of b, which means that a-b=0
+@return 1->yes 0->no
+*/
+int isAsub2B(FNode * aFNode, FNode * bFNode) {
+	FNode * resultNode = getSetDifference(aFNode, bFNode);
+	int flag = 0;
+	if (resultNode) {
+		flag = 1;
+	}
+	destroyAVL_F(resultNode);
+	return flag;
+}
+
+/**
+Set-remove
+Logistic : a=a-b
+@return a new A-node for updating data
+*/
+FNode * setRemove(FNode * aFNode, FNode * bFNode) {
+	FNode * resultNode = getSetDifference(aFNode, bFNode);
+	destroyAVL_F(aFNode);
+	return resultNode;
 }
 
 /*
 set_insert.
 Insert a-set into b-set.
-
-NOT USEFUL??
-
+WARNING: it would change b-set, if you wanna get a new tree to storage those sum-data then use getComFriend
 */
-//FNode * insertA2B(FNode * aFNode, FNode * bFNode) {
-//
-//}
-
+FNode * insertA2B(FNode * aFNode, FNode * bFNode) {
+	insertA2B_recursion(aFNode, bFNode);
+}
 /*
-Traverse a FNode-AVL-Tree, and insert all nodes into returning tree without check
-
+Traverse a FNode-AVL-Tree, and insert all nodes into sample tree
 */
-//void * insertA2B_recursion(FNode * aFNode, FNode * sampleRoot) {
-//	if (aFNode == NULL) {
-//		return;
-//	} else {
-//		insertAVL_F
-//	}
-//
-//}
+void * insertA2B_recursion(FNode * aFNode, FNode * sampleRoot) {
+	if (aFNode == NULL) {
+		return;
+	} else {
+		insertAVL_F(aFNode->info, sampleRoot, 0);
+		insertA2B_recursion(aFNode->left, sampleRoot);
+		insertA2B_recursion(aFNode->right, sampleRoot);
+	}
 
-
-/**
-set-remove
-*/
-
-/**
-set_subset
-*/
-
-/*
-set_diffrence
-*/
-
+}
